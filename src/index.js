d73d228b8a23be0f4b57dcb82329cb6a18ada43c
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-09-16 00:47:36 -0400

    switched from parcel to yeoman and blockstack generator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./App.js";
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.js';
 
-render(<App />, document.getElementById("root"));
+// Require Sass file so webpack can build it
+import 'bootstrap/dist/css/bootstrap.css';
+import'./styles/style.css';
+
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
